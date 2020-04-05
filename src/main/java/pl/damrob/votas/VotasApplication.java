983255d592a23bd8d6b5547d1b86e9c13974d4f3
Farius93 <robertwojcik1993@gmail.com>
2020-04-05 13:31:27 +0200

    Inital commit

---
+++ src/main/java/pl/damrob/votas/VotasApplication.java
@@ -0,0 +1,13 @@
+package pl.damrob.votas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/damrob/votas/VotasApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.damrob.votas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
