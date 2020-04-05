62d98e88cf866866e7f6e3a1e6d31b1cbd28dc3e
Robert Wójcik <robertwojcik1993@gmail.com>
2020-04-05 16:49:46 +0200

    Upgraded TestController

--- src/main/java/pl/damrob/votas/TestController.java
+++ src/main/java/pl/damrob/votas/TestController.java
@@ -7,8 +7,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping(value = "/test")
-    ResponseEntity<String> testMethod() {
-        return ResponseEntity.ok("Siemanko");
+    private static final String TEXT =
+            "__*♥♥♥*__*♥♥♥*___*♥♥♥*__*♥♥♥*\n" +
+            "_*♥♥♥*____*♥♥♥*_*♥♥♥*_____*♥♥♥*\n" +
+            "_*♥♥♥*________*♥♥♥*________*♥♥♥*\n" +
+            "*♥♥♥*_____Kocham Cie :*____*♥♥♥*\n" +
+            "_*♥♥♥*_____  Anetko  ______*♥♥♥*\n" +
+            "__*♥♥♥* _________________*♥♥♥*\n" +
+            "___*♥♥♥* ________________*♥♥♥*\n" +
+            "_____*♥♥♥* ___________*♥♥♥*\n" +
+            "_______*♥♥♥* _______ *♥♥♥*\n" +
+            "_________*♥♥♥*____*♥♥♥*\n" +
+            "__________*♥♥♥*__*♥♥♥*\n" +
+            "______________*♥♥♥*\n";
+
+    @GetMapping(value = "/kirwej")
+    ResponseEntity<String> kirwejMethod() {
+        return ResponseEntity.ok("Siemanko Kirwej, jestem online!, kiedy pijem?!");
+    }
+
+    @GetMapping(value = "/anetka")
+    ResponseEntity<String> anetkaMethod() {
+        return ResponseEntity.ok(TEXT);
     }
 }
