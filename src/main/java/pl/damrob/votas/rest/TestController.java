d93195c98a6290fcba6fa78cdec199ed68f41691
Robert Wójcik <robertwojcik1993@gmail.com>
2020-11-11 11:18:57 +0100

    Refactor

--- src/main/java/pl/damrob/votas/rest/TestController.java
+++ src/main/java/pl/damrob/votas/rest/TestController.java
@@ -9,28 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    private static final String TEXT =
-            "___*****_*****___*****__*****\n" +
-            "_*****____*****_*****_____*****\n" +
-            "*****________*****_________*****\n" +
-            "*****_____Kocham Cie :*____*****\n" +
-            "_*****_____  Anetko  ______*****\n" +
-            "__***** _________________*****\n" +
-            "___***** ________________*****\n" +
-            "_____***** ___________*****\n" +
-            "_______***** _______ *****\n" +
-            "_________*****____*****\n" +
-            "__________*****__*****\n" +
-            "______________*****\n";
-
     @GetMapping(value = "/kirwej")
     ResponseEntity<String> kirwejMethod() {
         return ResponseEntity.ok("Siemanko Kirwej, jestem online!, kiedy pijem?!");
-    }
-
-    @GetMapping(value = "/anetka")
-    ResponseEntity<String> anetkaMethod() {
-        return ResponseEntity.ok(TEXT);
     }
 
     @GetMapping(value = "/mati")
