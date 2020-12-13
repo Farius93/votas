bbb62122c00d8f54081db94f323a9e2486023845
Robert Wójcik <robertwojcik1993@gmail.com>
2020-12-13 20:29:15 +0100

    Temporay disable security

--- src/main/java/pl/damrob/votas/config/WebSecurityConfig.java
+++ src/main/java/pl/damrob/votas/config/WebSecurityConfig.java
@@ -60,7 +60,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/swagger-ui.html").permitAll()
                 .antMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 // all other requests need to be authenticated
-                .anyRequest().authenticated().and().
+                .anyRequest().permitAll().and(). //TODO temporary security is disabled
                 // make sure we use stateless session; session won't be used to
                 // store user's state.
                         exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
